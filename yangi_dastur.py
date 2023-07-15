0cbb5ef3a2b413cd164886ae58de6157fe277ee0
Avazbek <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-07-15 22:23:31 +0500

    python dasturlash tili

---
+++ yangi_dastur.py
@@ -0,0 +1,11 @@
+# def function bilan biz yangi funksiya yaratmoqchimiz endi bunda biz malumot = 
+# darajaga ko'tariladi va shunda biz bitta son kiritamiz va u sonni necha darajaga oshishini yozamiz
+# shunda bitta javob kelib chiqadi keyin biz shu javob tog'rimi yoki notog'ri ligini tekshiramiz 	
+
+def daraja(a,b):
+	print(a**b)
+
+d = int(input("Son kiriting: "))
+c = int(input("Darajaga oshadigan son: "))
+
+daraja(d,c)
